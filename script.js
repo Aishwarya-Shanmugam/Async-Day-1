f0a96eb6304181541461eb84c4afc2e56c5748b1
Aishwarya S <101408286+Aishwarya-Shanmugam@users.noreply.github.com>
2024-01-30 09:38:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+let seconds = 10;
+setTimeout(function() 
+{
+  var sec=  document.getElementById("second_col").innerText=seconds;
+  setTimeout(function() 
+  {
+   seconds=seconds-1;
+   var sec=  document.getElementById("second_col").innerText=seconds;
+     setTimeout(function ()
+     {
+      seconds=seconds-1;
+      var se= document.getElementById("second_col").innerText=seconds;
+        setTimeout(function() 
+        {
+         seconds=seconds-1;
+         var sec=  document.getElementById("second_col").innerText=seconds;
+           setTimeout(function() 
+           {
+            seconds=seconds-1;
+            var sec=  document.getElementById("second_col").innerText=seconds;
+              setTimeout(function() 
+              {
+               seconds=seconds-1;
+               var sec=  document.getElementById("second_col").innerText=seconds;
+                 setTimeout(function() 
+                 {
+                  seconds=seconds-1;
+                  var sec=  document.getElementById("second_col").innerText=seconds;
+                    setTimeout(function()
+                    {
+                     seconds=seconds-1;
+                     var sec=  document.getElementById("second_col").innerText=seconds;
+                       setTimeout(function()
+                       {
+                        seconds=seconds-1;
+                        var sec=  document.getElementById("second_col").innerText=seconds;
+                          setTimeout(function()
+                          {
+                           seconds=seconds-1;
+                           var sec=  document.getElementById("second_col").innerText=seconds;
+                             setTimeout(function()
+                             {
+                             document.getElementById("second_col").innerText="Happy Independence Day 🇮🇳";
+                             var img=document.createElement("text");
+                             var sec=  document.getElementById("second_col");
+                             sec.appendChild(Text);
+                    },1000);
+                  },1000);
+                },1000);
+              },1000);
+            },1000);
+          },1000);
+        },1000);
+      },1000);
+    },1000);
+  },1000);
+},0);
